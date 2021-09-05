863b3b1e56bf905a839f7367dac2bd0dde634db4
Batcleta <35137108+Batcleta@users.noreply.github.com>
2021-09-05 17:04:35 -0300

    implementação da primeira sessão

--- src/globalstyles/GlobalStyles.js
+++ src/globalstyles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { themes } from "./ColorStyles";
 
 export const Container = styled.div`
   padding: 1.5rem;
@@ -11,17 +12,117 @@ export const Container = styled.div`
   }
 `;
 
+//Global Text
 export const H1 = styled.h1`
   font-weight: bold;
   font-size: 2rem;
 `;
-
 export const H2 = styled.h2`
   font-weight: 600;
   font-size: 1.8rem;
 `;
-
 export const MediumText = styled.div`
   font-size: 1rem;
   line-height: 130%;
+`;
+
+//Global forms
+export const FormGroup = styled.div`
+  display: grid;
+  align-content: start;
+
+  @media (min-width: 1200px) {
+    transition: 0.5s;
+
+    &:nth-child(1) {
+      grid-row: 1 / 2;
+      grid-column: 1 / 2;
+
+      transform: ${({ comoSoube }) => (!comoSoube ? "translateY(7.1rem)" : "")};
+    }
+
+    &:nth-child(2) {
+      grid-row: 2 / 3;
+      grid-column: 2 / 3;
+    }
+
+    &:nth-child(3) {
+      grid-row: 2 / 3;
+      grid-column: 1 / 2;
+
+      opacity: ${({ comoSoube }) => (!comoSoube ? "1" : "0")};
+    }
+  }
+`;
+export const FormLabel = styled.div`
+  display: grid;
+  grid-template-columns: 0.93rem auto;
+
+  margin-bottom: 0.8rem;
+
+  font-weight: 600;
+  /* identical to box height */
+
+  color: rgba(0, 0, 0, 0.5);
+
+  > span {
+    color: ${themes.light.mainTextColor};
+    font-size: 1.125rem;
+    font-weight: 800;
+  }
+`;
+
+export const FormInput = styled.input`
+  padding: 0.875rem 1rem;
+  //max-width: 18.125rem;
+  height: 3.125rem;
+
+  /* Main Color */
+  border: 1px solid ${themes.light.mainColor};
+  box-sizing: border-box;
+  border-radius: 0.75rem;
+
+  font-weight: normal;
+  font-size: 0.95rem;
+  line-height: 1.125rem;
+
+  color: ${themes.light.menuLinkColor};
+
+  ::placeholder {
+    font-weight: normal;
+    font-size: 0.9rem;
+    line-height: 1.125rem;
+
+    color: ${themes.light.mainParColor};
+  }
+`;
+
+export const FormSelect = styled.select`
+  padding: 1rem;
+  height: 3.125rem;
+
+  /* Main Color */
+  border: 1px solid ${themes.light.mainColor};
+  box-sizing: border-box;
+  border-radius: 0.75rem;
+
+  font-size: 0.95rem;
+
+  color: ${themes.light.menuLinkColor};
+
+  ::placeholder {
+    font-weight: normal;
+    font-size: 0.9rem;
+    line-height: 1.125rem;
+
+    color: ${themes.light.mainParColor};
+  }
+`;
+
+export const ErrorMessage = styled.small`
+  color: red;
+  margin-top: 0.6rem;
+  margin-left: 1rem;
+
+  font-size: 0.9rem;
 `;
--- src/pages/subpages/PrimeiraEtapa.jsx
+++ src/pages/subpages/PrimeiraEtapa.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 
-import FormHeader from "../../components/templates/FormHeader";
 import { themes } from "../../globalstyles/ColorStyles";
 import { SwapButton } from "../../assets/images/SpecialIcons";
+import {
+  FormGroup,
+  FormInput,
+  FormLabel,
+  FormSelect,
+  ErrorMessage,
+} from "../../globalstyles/GlobalStyles";
 
-const PrimeiraEtapa = () => {
+//Components
+import FormHeader from "../../components/templates/FormHeader";
+
+const PrimeiraEtapa = (props) => {
   const {
     register,
     handleSubmit,
@@ -15,8 +24,11 @@ const PrimeiraEtapa = () => {
     setValue,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log("teste bem sucedido");
+  const comoSoube = watch("comoSoubeDaVaga") === "Indicação de funcionário";
+
+  const onSubmit = async (data) => {
+    await localStorage.setItem("comoSoube", JSON.stringify(data));
+    props.history.push("/formulario/informacoes-principais");
   };
 
   return (
@@ -24,37 +36,73 @@ const PrimeiraEtapa = () => {
       <FormWrapper>
         <FormHeader />
         <FormContent>
-          <TextWrapper>
-            <Title>Antes de começarmos</Title>
-            <Description>
+          <TextWrapper comoSoube={comoSoube}>
+            <Title comoSoube={comoSoube}>Antes de começarmos</Title>
+            <Description comoSoube={comoSoube}>
               Most coding courses treat design as an afterthought
             </Description>
           </TextWrapper>
           <ImageWrapper
             src={require("../../assets/images/web-form-image.svg").default}
           />
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <FormGroup>
-              <label>
+
+          <Form onSubmit={handleSubmit(onSubmit)} id={"myForm"}>
+            <FormGroup comoSoube={comoSoube}>
+              <FormLabel>
                 <span>*</span>
                 Como soube sobre a vaga?
-              </label>
-              <input type={"text"} />
+              </FormLabel>
+
+              <FormSelect
+                type="text"
+                placeholder="Escolha uma opção"
+                {...register("comoSoubeDaVaga", { required: true })}
+              >
+                <option value={""}>Escolha uma opção</option>
+                <option>Indicação de funcionário</option>
+                <option>Indicação de amigos</option>
+                <option>Whatsapp</option>
+                <option>Internet</option>
+              </FormSelect>
+              {errors.comoSoubeDaVaga && (
+                <ErrorMessage> Informe como soube da vaga</ErrorMessage>
+              )}
             </FormGroup>
+
             <FormGroup>
-              <label>
+              <FormLabel>
                 <span>*</span>
-                Nome do funcionário que fez a indicação
-              </label>
-              <input type={"text"} />
+                Qual a sua pretenção salarial?
+              </FormLabel>
+              <FormInput
+                type="text"
+                {...register("pretencaoSalarial", { required: true })}
+              />
+              {errors.pretencaoSalarial && (
+                <ErrorMessage>Informe sua pretenção salarial</ErrorMessage>
+              )}
             </FormGroup>
-            <FormGroup>
-              <label>
-                <span>*</span>
-                Como soube sobre a vaga?
-              </label>
-              <input type={"text"} />
-            </FormGroup>
+
+            {!comoSoube ? (
+              ""
+            ) : (
+              <FormGroup>
+                <FormLabel>
+                  <span>*</span>
+                  Qual o nome do funcionário?
+                </FormLabel>
+                <FormInput
+                  type="text"
+                  placeholder="Digite o nome do funcionário"
+                  {...register("nomeDoFuncionarioQueIndicou", {
+                    required: true,
+                  })}
+                />
+                {errors.nomeDoFuncionarioQueIndicou && (
+                  <ErrorMessage>Informe o nome do funcionário</ErrorMessage>
+                )}
+              </FormGroup>
+            )}
 
             <DescribeBetter>
               <FormTextWrapper>
@@ -64,12 +112,25 @@ const PrimeiraEtapa = () => {
                   seletivo
                 </p>
               </FormTextWrapper>
-              <textarea />
+              <div style={{ display: "grid" }}>
+                <textarea
+                  name=""
+                  placeholder="Informe aqui"
+                  {...register("motivoDaParticipacao", { required: true })}
+                />
+                {errors.motivoDaParticipacao && (
+                  <ErrorMessage>Informe sua motivação</ErrorMessage>
+                )}
+              </div>
             </DescribeBetter>
-            <NextButton type={"submit"} />
           </Form>
         </FormContent>
       </FormWrapper>
+
+      <NextButton form={"myForm"}>
+        Seguinte
+        <SwapButton />
+      </NextButton>
     </Wrapper>
   );
 };
@@ -90,7 +151,6 @@ const FormWrapper = styled.div`
 
   border-radius: 1rem;
 `;
-
 const FormContent = styled.div`
   display: grid;
   gap: 3.125rem;
@@ -105,28 +165,35 @@ const FormContent = styled.div`
     padding: 2rem 2rem;
   }
 `;
-
 const TextWrapper = styled.div`
+  transition: 0.5s;
+
   @media (min-width: 1200px) {
     grid-column: 1 / 2;
     grid-row: 1 / 2;
+
+    transform: ${({ comoSoube }) => (!comoSoube ? "translateY(5rem)" : "")};
   }
 `;
 const Title = styled.div`
   font-weight: 800;
-  font-size: 1.375rem;
+  transition: 0.5s;
+
+  font-size: ${({ comoSoube }) => (!comoSoube ? "1.5rem" : "1.375rem")};
+
   line-height: 1.75rem;
   color: ${themes.light.mainTextColor};
 `;
 const Description = styled.div`
   font-weight: normal;
-  font-size: 1.05rem;
+  transition: 0.5s;
+
+  font-size: ${({ comoSoube }) => (!comoSoube ? "1.2rem" : "1.05rem")};
   line-height: 130%;
   /* or 22px */
 
   color: ${themes.light.menuLinkColor};
 `;
-
 const ImageWrapper = styled.img`
   display: none;
 
@@ -137,8 +204,7 @@ const ImageWrapper = styled.img`
     grid-row: 1 / 3;
   }
 `;
-
-const Form = styled.div`
+const Form = styled.form`
   display: grid;
   gap: 2.2rem;
 
@@ -150,83 +216,21 @@ const Form = styled.div`
     grid-template-rows: repeat(3, auto);
   }
 `;
-
-const FormGroup = styled.div`
-  display: grid;
-  gap: 0.93rem;
-
-  > label {
-    display: grid;
-    grid-template-columns: 0.93rem auto;
-    font-weight: 600;
-    font-size: 0.875rem;
-    line-height: 1.125rem;
-    /* identical to box height */
-
-    color: rgba(0, 0, 0, 0.5);
-
-    > span {
-      color: ${themes.light.mainTextColor};
-      font-size: 1.125rem;
-      font-weight: 800;
-    }
-  }
-  > input {
-    padding: 0.875rem 1rem;
-    max-width: 18.125rem;
-    height: 3.125rem;
-
-    /* Main Color */
-
-    border: 1px solid ${themes.light.mainColor};
-    box-sizing: border-box;
-    border-radius: 0.75rem;
-
-    font-weight: normal;
-    font-size: 0.875rem;
-    line-height: 1.125rem;
-    /* identical to box height */
-
-    color: rgba(0, 0, 0, 0.9);
-
-    ::placeholder {
-      font-weight: normal;
-      font-size: 0.875rem;
-      line-height: 1.125rem;
-      /* identical to box height */
-
-      color: rgba(0, 0, 0, 0.9);
-    }
-  }
-
-  @media (min-width: 1200px) {
-    &:nth-child(1) {
-      grid-row: 1 / 2;
-      grid-column: 1 / 2;
-    }
-
-    &:nth-child(2) {
-      grid-row: 2 / 3;
-      grid-column: 2 / 3;
-    }
-
-    &:nth-child(3) {
-      grid-row: 2 / 3;
-      grid-column: 1 / 2;
-    }
-  }
-`;
-
 const DescribeBetter = styled.div`
   display: grid;
   gap: 2.2rem;
+
   textarea {
-    max-width: 18.125rem;
+    //max-width: 18.125rem;
     height: 6.875rem;
+
+    padding: 1rem;
 
     border: 1.2px solid ${themes.light.mainColor};
     box-sizing: border-box;
     border-radius: 0.75rem;
+
+    font-size: 1rem;
   }
 
   @media (min-width: 1200px) {
@@ -252,8 +256,7 @@ const FormTextWrapper = styled.div`
     color: rgba(0, 0, 0, 0.6);
   }
 `;
-
-const NextButton = styled.input`
+const NextButton = styled.button`
   position: absolute;
   right: 0;
   margin-top: 2rem;
